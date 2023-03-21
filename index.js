6e82a0aa987bf18430e5ea908968b8c249e4916e
heathbaldwin <heathabaldwin@gmail.com>
2023-03-20 20:30:38 -0400

    changes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add JavaScript below
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.8.0"
+
+const supabaseUrl = 'https://fysqhacvpzkquaqnsabq.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5c3FoYWN2cHprcXVhcW5zYWJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1MzY2MTMsImV4cCI6MTk5MzExMjYxM30.2bhtVZU7Zv55bLCKsxFDNK0QQ9bk5LqtDEXEq2iZS-o'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getBooks() {
+let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+for (let book of books) {
+  let bookList = document.getElementById('books');
+  bookList.innerHTML += `<tr>${book.title} - ${book.author} - ${book.ISBN} </br></tr>`;
+  }
+}
+getBooks();
